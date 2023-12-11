56ad1a6ed5c45a910151d3744652bff822292bc1
fayzullohblog <fayzulloh00010003@gmail.com>
2023-12-11 16:12:40 +0500

    a

--- accountapp/manager.py
+++ accountapp/manager.py
@@ -31,6 +31,7 @@ class UserManager(BaseUserManager):
         )
 
         user.is_superuser=True
+        print('assasas')
         user.is_admin=True
         user.is_active=True
         user.save(using=self._db)
