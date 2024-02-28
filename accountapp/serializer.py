97db81a202f6c61ea1cfcd268fea7adf84e81d38
fayzullohblog <fayzulloh00010003@gmail.com>
2024-02-28 11:01:07 +0500

    d

--- accountapp/serializer.py
+++ accountapp/serializer.py
@@ -2,23 +2,41 @@ from rest_framework_simplejwt import serializers
 from rest_framework_simplejwt.tokens import Token
 from rest_framework.serializers import ModelSerializer
 from .models import MyUser
-
+import jwt
+from decouple import config
 
 class MyTokenObtainPairSerializer(serializers.TokenObtainPairSerializer):
     
     @classmethod
     def get_token(cls, user) -> Token:
         token=super().get_token(user)
+        print('------------>',user.is_boss)
         token['username']=user.username
-        # token['is_admin']=user.is_admin
         token['is_staff']=user.is_staff
         token['is_superuser']=user.is_superuser
         token['is_active']=user.is_active
-        token['is_boss']=user.is_active
+        token['is_boss']=user.is_boss
+        
+        token_string = str(token)
 
-        return token
-        
-        
+ 
+
+        # Decode the token
+        decoded_token = decode_jwt_access_token(token_string)
+        print('----------->', decoded_token)
+        return token     
+
+def decode_jwt_access_token(token):
+    try:
+        decoded_token = jwt.decode(token, config('SECRET_KEY'), algorithms=['HS256'])
+        return decoded_token
+    except jwt.ExpiredSignatureError:
+        # Handle expired token
+        return {"error": "Token has expired"}
+    except jwt.InvalidTokenError:
+        # Handle invalid token
+        return {"error": "Invalid token"}
+    
 
 class MyUserSerializer(ModelSerializer):
     class Meta:
