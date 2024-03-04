812737122c37f1f8efd207a878e49781316a83e2
fayzullohblog <fayzulloh00010003@gmail.com>
2024-03-04 14:03:02 +0500

    d

--- config/settings.py
+++ config/settings.py
@@ -224,6 +224,10 @@ STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 # django 4.2+ whitenoise 
 STORAGES = {
     # ...
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
